b66513b89c4a82e784d2b74d28f85ed9102bea65
Mario <ojeifomario@gmail.com>
2022-03-19 01:05:12 -0700

    My variables commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,6 +9,7 @@ int main(void)
 {
 int number;
 
+
 for (number = 48; number <= 57; number++)
 {
 putchar(number);
@@ -19,5 +20,11 @@ break;
 }
 
 putchar(',');
-putchar(' ')'
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+
 }
