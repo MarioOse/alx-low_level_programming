dc280f8f5db45b876863ebfa08bd33fd557dcf3f
Mario <ojeifomario@gmail.com>
2022-03-24 09:09:07 -0700

    My commit message

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_strcmp - compares two strings
- *@s1: A pointer to a character that will be changed
- *@s2: A pointer to a character that will also be changed/modified/updated
+ * @s1: A pointer to a character that will be changed
+ * @s2: A pointer to a character that will also be changed/modified/updated
  * Return: dest
  */
 
@@ -14,7 +14,7 @@ char *str_one = s1;
 char *str_two = s2;
 
 
-while (*str_one != '0\' && *str_two != '\0' && *str_one == *str_two)
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 {
 str_one++;
 str_two++;
