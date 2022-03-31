22d9dd95f55606e5db2238cafa7a6db2998dc48d
Mario <ojeifomario@gmail.com>
2022-03-30 18:05:29 -0700

    my commit for 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
- * Return: pointer
-void _puts_recursion(char *s)
-{
-        int i = 0;
+ * _puts_recursion - prints a string
+ * @s: pointer block of memory to fill
+ * Return: void
+ */
 
-        if (s[i] != '\0')
-        {
-                _putchar(s[i]);
-                _puts_recursion(s + 1);
-        }
-        else
-                _putchar('\n');
+void _puts_recursion(char *s)
+
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_putchar (*s);
+_puts_recursion(s + 1);
+
 }
