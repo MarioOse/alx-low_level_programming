e68f134501c501a5bd984b8eca24d2071534b733
Mario Ojeifo <ojeifomario@gmail.com>
2022-06-13 23:01:45 +0000

    My commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,7 @@
-#!/bin/bash 
-wget -P /tmp https://github.com/MarioOse/alx-low_level_programming.git/raw/main/0x18-dynamic_libraries/libmask.so 
+#!/bin/bash  
+wget -P /tmp https://github.com/MarioOse/alx-low_level_programming.git/raw/main/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so 
+chmod u+x 101-make_me_win.sh
+git add .
+git commit -m "My commit message"
+git push
